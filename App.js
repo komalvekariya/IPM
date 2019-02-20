fc105ed3f5c60475676b023fa319a9814404b2fb
Riddhi <Riddhi@bash.local>
2019-02-20 19:34:53 +0530

    Logo Added

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 import {  StyleSheet, Text, View } from 'react-native';
-import Login from './src/screens/login'
+import Login from './src/screens/Login'
 export default class App extends Component {
   render() {
     return (
@@ -14,7 +14,7 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex:1
   },
 
 });
--- src/screens/login.js
+++ src/screens/login.js
@@ -1,18 +1,28 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
-
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient'
+import LinearTextGradient  from 'react-native-text-gradient';
 // create a component
 class Login extends Component {
+
     render() {
         return (
             <View style={styles.container}>
-                <View style={{ alignItems: 'center' }}>
-                    <Text style={{ color: 'red', fontSize:100, paddingTop: 50 }}>Login</Text>
-                </View>
-                <View style={{ alignItems: 'center',justifyContent:'center',flex:1}}>
-                    <TextInput style={{ borderBottomColor: 'black', borderBottomWidth: 1, width: '80%', fontSize:20}} placeholder='UserName' />
-                    <TextInput style={{ borderBottomColor: 'black', borderBottomWidth: 1, width: '80%', fontSize:20}} placeholder='Password' />
+                <View style={{ flex: 1, paddingTop: 60, justifyContent: 'center', alignItems: 'center' }} elevation={5}>
+                    <LinearGradient
+                        colors={['#00FFFF', '#17C8FF', '#329BFF', '#4C64FF', '#6536FF', '#8000FF']}
+                        start={{ x: 0.0, y: 1.0 }} end={{ x: 1.0, y: 1.0 }}
+                        style={{ height: '10%', width: '90%', justifyContent: 'center', alignItems: 'center', }}
+                    >
+                        <View style={{backgroundColor: 'white', width: '97%', height: '90%', justifyContent: 'center', alignItems: 'center' }}>
+                            <Text style={{ fontSize: 40, fontWeight: "bold", color: '#6536FF',backgroundColor: 'transparent' }}>Login</Text>
+                        </View>
+                    </LinearGradient>
+                    <View style={{ elevation:4,flex: 1, justifyContent: "space-evenly", alignItems: 'center', width: '90%' }} >
+                        <TextInput style={{ borderBottomColor: '#000000', borderBottomWidth: 1, width: '80%', fontSize: 20 }} placeholder='UserName' />
+                        <TextInput style={{ borderBottomColor: '#000000', borderBottomWidth: 1, width: '80%', fontSize: 20 }} placeholder='Password' />
+                    </View>
                 </View>
             </View>
         );
@@ -23,8 +33,11 @@ class Login extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#F0F0F0'
+        flexDirection: 'column',
+        backgroundColor: '#FFFFFF',
+        
     },
+
 });
 
 //make this component available to the app
