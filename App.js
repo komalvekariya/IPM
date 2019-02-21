af809b60d19cb01608bc1988a35675390fb17a60
Maheshbokhani <mahesh.bokhani@bacancytechnology.com>
2019-02-21 17:38:04 +0530

    IPM

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 import {  StyleSheet, Text, View } from 'react-native';
-import Login from './src/screens/Login'
+import Login from './src/screens/login'
 export default class App extends Component {
   render() {
     return (
