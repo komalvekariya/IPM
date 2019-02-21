d2e00f66d15d13ba1526566bde045813ea0b59a7
komalvekariya <komal.vekariya@bacancytechnology.com>
2019-02-21 19:54:24 +0530

    Make Responsive2

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -37,6 +37,9 @@ class Login extends Component {
                         <TouchableOpacity style={styles.button}>
                             <Text style={{ fontSize: 30, padding: 6, color: '#FFFF' }}>Login</Text>
                         </TouchableOpacity>
+                        <TouchableOpacity style={styles.button}>
+                            <Text style={{ fontSize: 30, padding: 6, color: '#FFFF' }}>Login</Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
             </ScrollView>
