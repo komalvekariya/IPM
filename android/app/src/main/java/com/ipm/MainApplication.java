9dbcbeb56a550c21fdec02fadb2f7984e1e6c86a
Riddhi <Riddhi@bash.local>
2019-02-22 15:09:45 +0530

    Auth

--- android/app/src/main/java/com/ipm/MainApplication.java
+++ android/app/src/main/java/com/ipm/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,7 +29,9 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
             new RNTextGradientPackage(),
             new RNFirebasePackage(),
-            new LinearGradientPackage()
+            new LinearGradientPackage(),
+            new RNFirebaseAuthPackage()
+
       );
     }
 
