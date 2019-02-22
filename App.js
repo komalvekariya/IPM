7c0f4f1ed86d1a9f092f7aca9134630c909c2c68
komalvekariya <komal.vekariya@bacancytechnology.com>
2019-02-22 15:27:12 +0530

    okdone

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 
 import React, { Component } from 'react';
 import {  StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
-import Login from './src/screens/login'
-=======
 import Login from './src/screens/Login'
 import Dashboard from './src/screens/Dashboard'
->>>>>>> 9ca9af6af41d4951d61c3e46e8c241cd0fc82a90
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Login />
+        <Dashboard/>
       </View>
     );
   }
