83b6510797450069fb48cb2d9fff2016f7de64bc
Harsh <harsh.patel@bacancytechnology.com>
2019-02-22 15:47:46 +0530

    Modal added

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,11 +1,21 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableHighlight, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Modal } from 'react-native';
 import * as data from '../demodata.json'
 import { BoxShadow } from 'react-native-shadow'
 
 // create a component
 class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: 'All',
+            ModalVisible: false
+        }
+    }
+    onFilterClick(status) {
+        this.setState({ status: status })
+    }
     render() {
         const shadowOpt = {
             width: 160,
@@ -19,7 +29,37 @@ class Dashboard extends Component {
             style: { marginVertical: 5 }
         }
         return (
+
             <View style={styles.container}>
+                {/* Modal start */}
+                <Modal
+                    visible={this.state.ModalVisible}
+                    transparent={true}
+                >
+                    <View style={{ flex: 1 }}>
+                        <View style={{ flex: 2, }}>
+                            <TouchableOpacity
+                                style={{ flex: 1, backgroundColor: '#DDDDDD99' }}
+                                onPress={() => this.setState({ModalVisible: !this.state.ModalVisible}) }
+                            ></TouchableOpacity>
+                        </View>
+                        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'red', flexDirection: 'column' }}>
+                            <TouchableOpacity
+                                style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                                <Text style={{ fontSize: 25, marginHorizontal: 10, fontWeight: '900' }}>All</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                                <Text style={{ fontSize: 25, marginHorizontal: 10, fontWeight: '900' }}>Pending</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                                <Text style={{ fontSize: 25, marginHorizontal: 10, fontWeight: '900' }}>Selected</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </Modal>
+                {/*   Modal End  */}
                 {/* <BoxShadow setting={shadowOpt}> */}
                 <TouchableOpacity style={styles.TouchableOpacityStyle} >
                     <Text style={{ fontSize: 30, fontWeight: '900' }}>+</Text>
@@ -29,9 +69,16 @@ class Dashboard extends Component {
                     <Text style={{ flex: 1.5, fontSize: 30, marginHorizontal: 20, fontWeight: 'bold' }}>Welcome</Text>
                     <Text style={{ flex: 1, fontSize: 30, marginHorizontal: 10, fontWeight: 'bold' }}>username</Text>
                 </View>
-                <View style={{ flex: .07, flexDirection: 'row', alignItems: 'center'/* , backgroundColor: 'gray' */ }}>
-                    <Text style={{ flex: 1.5, fontSize: 25, marginHorizontal: 20 , fontWeight:'900'}}>Name</Text>
-                    <Text style={{ flex: 1, fontSize: 25, marginHorizontal: 10,fontWeight: '900' }}>Status</Text>
+                <View style={{ flex: .07, flexDirection: 'row', alignItems: 'center', backgroundColor: 'gray', justifyContent: 'space-around' }}>
+                    <Text style={{ flex: 1, fontSize: 25, marginHorizontal: 20, fontWeight: '900' }}>Name</Text>
+                    <TouchableOpacity
+                        style={{ flexDirection: 'row', flex: 1, alignItems: 'center' }}
+                        onPress={() => this.setState({ModalVisible: true}) }
+
+                    >
+                        <Text style={{ fontSize: 25, marginHorizontal: 10, fontWeight: '900' }}>Status</Text>
+                        <Text style={{ fontSize: 20, marginHorizontal: 5, fontWeight: '500' }}>{this.state.status}</Text>
+                    </TouchableOpacity>
                 </View>
                 <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', }}>
 
